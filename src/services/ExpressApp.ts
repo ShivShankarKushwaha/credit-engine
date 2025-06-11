09d2d6e9325117e18b3f831d74f4cba11984df13
ShivShankarKushwaha <shivshankarkushwaha0000@gmail.com>
2025-06-11 17:58:23 +0530

    fix: vercel deployment

--- src/services/ExpressApp.ts
+++ src/services/ExpressApp.ts
@@ -5,7 +5,7 @@ import { middleware } from '../middlewares';
 export const App = (app: Application) => {
 	middleware(app);
 	app.get('/', (req, res) => {
-		res.status(200).json({ message: 'Welcome to the CREDIT ENGINE API. see more details here https://github.com/ShivShankarKushwaha/credit-engine' });
+		return res.status(200).json({ message: 'Welcome to the CREDIT ENGINE API. see more details here https://github.com/ShivShankarKushwaha/credit-engine' });
 	});
 	app.use('/api', mainRoute);
 
